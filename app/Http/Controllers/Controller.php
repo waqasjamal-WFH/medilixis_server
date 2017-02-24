cc000cb839ee1b32d2c2b68f14dc19c5238c2373
waqasjamal <waqasjamal.wfh@gmail.com>
2017-02-24 18:27:34 +0500

    controller and model

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,8 +9,10 @@ use Illuminate\Http\Request;
 
 use App\restfulModel;
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+
 use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+// use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use DB;
 use Session;
 class Controller extends BaseController
