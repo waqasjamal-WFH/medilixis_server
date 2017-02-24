f5013326295680fcb4515d45acbd2de1fc92ab58
waqasjamal <waqasjamal.wfh@gmail.com>
2017-02-24 19:23:13 +0500

    controller and model

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,7 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 // use Illuminate\Http\Request;
 
 
-// use App\restfulModel;
+use App\restfulModel;
 // use Illuminate\Foundation\Bus\DispatchesJobs;
 // use Illuminate\Foundation\Validation\ValidatesRequests;
 // use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
