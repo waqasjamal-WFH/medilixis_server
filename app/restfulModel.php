600d843a78b4d30772e40879238293af50855b7a
waqasjamal <waqasjamal.wfh@gmail.com>
2017-03-10 18:34:01 +0500

    login api work

--- app/restfulModel.php
+++ app/restfulModel.php
@@ -74,7 +74,7 @@ class restfulModel extends Model
 
 
 $user = DB::table('users')
-            ->leftJoin('roles', 'users.role_id', '=', 'roles.id')
+            ->join('roles', 'users.role_id', '=', 'roles.id')
             ->where('email','=', $data->email)
            ->where('password','=', md5($data->password))
            ->first();
