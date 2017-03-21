6b9e0d7578ccc5d0e96ca0d392293a544158cb7d
waqasjamal <waqasjamal.wfh@gmail.com>
2017-03-21 17:09:57 +0500

    waqas

--- app/restfulModel.php
+++ app/restfulModel.php
@@ -273,7 +273,7 @@ class restfulModel extends Model
             $keyvalue[$key]=$va;
             $i++;
         };
-        print_r($keyvalue);
+        // print_r($keyvalue);
 
         // //token generate function
         // $token = "";
@@ -284,23 +284,23 @@ class restfulModel extends Model
         //     $token .= $codeAlphabet[$this->crypto_rand_secure(0,strlen($codeAlphabet))];
         // }
 
-       // $lastid= DB::table('users')->insertGetId([
-       //      'username' => $keyvalue["username"], 'password' => md5($keyvalue["password"]), 'email' => $keyvalue["email"],'role_id' =>$keyvalue["role_id"]       ]);
-       //  $token =DB::table('users')->where('id', '=', $lastid)->pluck('token');
-       //  unset($keyname['username']);
-       //  unset($keyvalue['username']);
-       //  unset($keyname['password']);
-       //  unset($keyvalue['password']);
-       //  unset($keyname['email']);
-       //  unset($keyvalue['email']);
+       $lastid= DB::table('users')->insertGetId([
+            'username' => $keyvalue["username"], 'password' => md5($keyvalue["password"]), 'email' => $keyvalue["email"],'role_id' =>$keyvalue["role_id"]       ]);
+        $token =DB::table('users')->where('id', '=', $lastid)->pluck('token');
+        unset($keyname['username']);
+        unset($keyvalue['username']);
+        unset($keyname['password']);
+        unset($keyvalue['password']);
+        unset($keyname['email']);
+        unset($keyvalue['email']);
 
-       //  foreach($keyname as $kn=>$kv){
-       //      DB::table('userdetails')->insert([
-       //          'key_name' => $kn, 'key_value' => $keyvalue[$kv],'user_id'=>$lastid
-       //      ]);
+        foreach($keyname as $kn=>$kv){
+            DB::table('userdetails')->insert([
+                'key_name' => $kn, 'key_value' => $keyvalue[$kv],'user_id'=>$lastid
+            ]);
 
-       //  }
-       //  return array('result'=>"true", 'token'=>$token);
+        }
+        return array('result'=>"true", 'token'=>$token);
   }
 
   //.......................add tranco admin model end here............................
