54c4cfcb3d1812e5b9dd9bc47c758e8500004d18
waqasjamal <waqasjamal.wfh@gmail.com>
2017-03-27 18:08:15 +0500

    waqas

--- app/restfulModel.php
+++ app/restfulModel.php
@@ -414,7 +414,8 @@ $arr=array();
     foreach ($user as $users) {
       $nav_permission = DB::table('nav_permission')->where('user_id','=', $users->userID)->get();
       $arrr=array();
-      // $nav_object=$nav_permission->0;
+      $nav_object=$nav_permission;
+      $users->permission=implode(">>>", $nav_permission[0]);
       // foreach ($nav_object as $key => $value) {
       //   if($value== "1"){
       //     $arrr[]=$key;
@@ -426,7 +427,7 @@ $arr=array();
       // print_r($arrr);
       // $users->nav_permissions=$nav_permission;
     };
-var_dump($nav_permission);
+var_dump($user);
     
     // if($user){
     //     return array('result'=>"true", 'token'=>$data->token , 'data'=> $user);
